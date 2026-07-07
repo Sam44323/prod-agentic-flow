c3d65372606f413b5b533ef4e044dd0ed772ddaa
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-08 02:23:50 +0530

    add: added the wrapper version for qwen

--- app/llm/qwen.py
+++ app/llm/qwen.py
@@ -2,7 +2,7 @@ from langchain_ollama import ChatOllama
 from app.config import MODEL_NAME, OLLAMA_BASE_URL
 
 # creating an LLM-Wrapper to avoid duplicating code
-llm = ChatOllama(model_name=MODEL_NAME, base_url=OLLAMA_BASE_URL)
+llm = ChatOllama(model=MODEL_NAME, base_url=OLLAMA_BASE_URL)
 
 # getting the LLM instance for the interaction
 def get_llm():
