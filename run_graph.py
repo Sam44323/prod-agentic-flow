b6e3f504f86f9f0961b0f9ecf993766ec1f21c15
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-08 15:37:12 +0530

    add: added multi-variant asks

--- run_graph.py
+++ run_graph.py
@@ -3,5 +3,6 @@ from app.graph.graph import app
 
 
 result = app.invoke({"user_input": "2 + 2", "final_answer": ""})
+result = app.invoke({"user_input": "what is lang-graph?", "final_answer": ""})
 
 print(result)
