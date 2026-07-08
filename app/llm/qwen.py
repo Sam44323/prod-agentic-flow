1f32165caba2cf648815bcc4373507fe17d6ac7e
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-09 01:26:06 +0530

    add: added the str-casting

--- app/llm/qwen.py
+++ app/llm/qwen.py
@@ -2,7 +2,8 @@ from langchain_ollama import ChatOllama
 from app.config import MODEL_NAME, OLLAMA_BASE_URL
 
 # creating an LLM-Wrapper to avoid duplicating code
-llm = ChatOllama(model=MODEL_NAME, base_url=OLLAMA_BASE_URL)
+llm = ChatOllama(model=str(MODEL_NAME), base_url=OLLAMA_BASE_URL)
+
 
 # getting the LLM instance for the interaction
 def get_llm():
