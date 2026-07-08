374d8e41f72574721477f92cf2cc195bf9934454
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-08 15:49:20 +0530

    add: added the configs for pyrightconfig

--- run_graph.py
+++
@@ -1,8 +0,0 @@
-# temp-file for testing
-from app.graph.graph import app
-
-
-result = app.invoke({"user_input": "2 + 2", "final_answer": ""})
-result = app.invoke({"user_input": "what is lang-graph?", "final_answer": ""})
-
-print(result)
