b1109f0e4a03b035ea1d4505d9e9d4f072169cfc
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-08 15:35:40 +0530

    add: added temp-loggers for the nodes and the routers

--- app/graph/nodes.py
+++ app/graph/nodes.py
@@ -5,10 +5,12 @@ from app.llm.qwen import get_llm
 # LLM-Node for query and response with the language-model
 def llm_node(state: AgentState) -> AgentState:
     llm = get_llm()
+    print(state)
 
     response = llm.invoke(state["user_input"])
 
     state["final_answer"] = response.content
+    print("exiting the llm_node")
 
     return state
 
--- app/graph/router.py
+++ app/graph/router.py
@@ -3,9 +3,12 @@ from app.graph.state import AgentState
 
 # routing the flow based on the user-input
 def router(state: AgentState):
+    print("Routing...")
     query = state["user_input"]
 
     if "+" in query:
+        print("routing to calculator")
         return "calculator"
 
+    print("routing to llm")
     return "llm"
