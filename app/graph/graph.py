58f14d1cda6bc897cd5327c7724dc2e0f8bb9137
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-08 15:32:28 +0530

    add: added the fallback comments

--- app/graph/graph.py
+++ app/graph/graph.py
@@ -7,6 +7,7 @@ from app.graph.router import router
 # This is telling lang-graph that every node in the flow would be using the AgentState
 graph = StateGraph(AgentState)
 
+# nodes for wrking
 graph.add_node("llm", llm_node)
 graph.add_node("calculator", calculator_node)
 
@@ -19,11 +20,14 @@ graph.add_node("calculator", calculator_node)
 # Router: "+" in input → calculator, else → llm
 
 # conditional-edges: route from START, using router() to decide,
-# mapping its return value ("llm"/"calculator") to actual node names
+# mapping its return-value ("llm"/"calculator") to actual node names
 graph.add_conditional_edges(
     START,
     router,
-    {"calculator": "calculator", "llm": "llm"},
+    {
+        "calculator": "calculator",
+        "llm": "llm",
+    },  # this is not needed here as return-values are same-name to nodes but just in-case
 )
 
 graph.add_edge("llm", END)
@@ -32,4 +36,3 @@ graph.add_edge("calculator", END)
 
 # compiling the graph
 app = graph.compile()
-
