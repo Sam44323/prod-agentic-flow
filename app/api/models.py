c6c5a662542a993222e352033dd9e14d64b1f9d1
Suranjan <suranjanbiswas2002@gmail.com>
2026-07-09 17:45:07 +0530

    update: updated the models

--- app/api/models.py
+++ app/api/models.py
@@ -1,9 +1,24 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
+# request for the interaction of the api
 class ChatRequest(BaseModel):
     message: str
 
 
+# response for the api
 class ChatResponse(BaseModel):
-    answer: str
+    answer: Optional[str] = None
+
+    approval_required: bool = False
+    thread_id: Optional[str] = None
+    approval_message: Optional[str] = None
+    approval_reason: Optional[str] = None
+
+
+# interrruption-information request
+class ApprovalRequest(BaseModel):
+    thread_id: str
+    approved: bool
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,15 +1,40 @@
 from fastapi import APIRouter
+import uuid
+
+from langchain_core.runnables import RunnableConfig
 
 from app.api.models import ChatRequest, ChatResponse
 from app.graph.graph import app as graph
+from app.graph.state import AgentState  # wherever AgentState is defined
 
 router = APIRouter()
 
 
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
+    thread_id = str(uuid.uuid4())
 
-    # NOTE: in this invoke() initial dict keys must match AgentState keys overall exactly:
-    result = graph.invoke({"user_input": request.message, "final_answer": ""})
+    config: RunnableConfig = {
+        "configurable": {
+            "thread_id": thread_id,
+        }
+    }
+
+    initial_state: AgentState = {
+        "user_input": request.message,
+        "final_answer": "",
+        "messages": [],
+        "tool_name": "",
+        "tool_input": "",
+        "tool_output": "",
+        "error": "",
+        "approval_required": False,
+        "approval_granted": False,
+        "approval_reason": "",
+    }
+
+    # in this we are thread_id to every execution so that we can use it a reference for resuming
+    result = graph.invoke(initial_state, config=config)
 
     return ChatResponse(answer=result["final_answer"])
+
